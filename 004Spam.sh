fc317de8be111af1d32173e9c6d49806b3835da1
Mr.004 <57171003+Mr-004Tols@users.noreply.github.com>
2019-11-06 15:33:38 +0700

    004Spam

---
+++ 004Spam.sh
@@ -0,0 +1,171 @@
+import os, sys
+
+print ("\033[1;32mSilahkan Masukkan Username & Password Anda")
+
+print ("\033[1;32matau silahkan Hubungi wa 081228244242")
+
+username = 'Mr_004'      
+
+password = 'Dia Newbie'
+
+
+
+def restart():
+
+	ngulang = sys.executable
+
+	os.execl(ngulang, ngulang, *sys.argv)
+
+
+
+def main():
+
+	uname = raw_input("username : ")
+
+	if uname == username:
+
+		pwd = raw_input("password : ")
+
+
+
+		if pwd == password:
+
+			print "\033[1;32mAlhmdllh sudah masuk juga..", 
+
+			sys.exit()
+
+
+
+		else:
+
+			print "\033[1;32mMaaf Masukkan password Anda salah... [?]\033[00m"
+
+			print "Silahkan segera log-in kembali...!!\n"
+
+			restart()
+
+
+
+	else:
+
+		print "\033[1;32mMaaf Masukkan Username Anda salah... [?]\033[00m"
+
+		print "Silahkan segera log-in kembali...!!\n"
+
+		restart()
+
+
+
+try:
+
+	main()
+
+except KeyboardInterrupt:
+
+	os.system('clear')
+
+	restart()
+clear
+toilet -f bigmono9 -F gay 004 Spam
+sleep 1
+echo "\033[38;1m ------------------------------------------------------------------"|lolcat
+echo "\033[31;1m #Gamer 4 LIve "
+echo "\033[36;1m author by :"
+echo "\033[36;1m Mr.004 | Hat_12 | Conor9k"
+echo "\033[32;1m install bahan dlu asw kalau ngga install ngga bisa (08)"
+echo "\033[38;1m ------------------------------------------------------------------"|lolcat
+
+date|lolcat
+echo ""
+sleep 1
+     echo "\033[34;1m SPAM Yang Tersedia "
+     echo "   '(01)'JDID "
+     echo "   '(02)'TELKOMSEL "
+     echo "   '(03)'PHD.      "
+     echo "   '(04)'MATHARIMALL"
+     echo "   '(05)'GRAB "
+     echo "   '(06)'TOKOPEDIA"
+sleep 1
+echo 
+     echo "    (07)INFO "
+     echo "    (08)Install Bahan"
+     echo "    (09)Keluar "
+echo " @root.mr.004 "
+read ezz
+if [ $ezz = 1 ] || [ $ezz = 01 ]
+then
+clear
+echo "\033[31;1m"
+toilet -f mono12 -F gay "JDID"
+php jdid.php
+fi
+
+if [ $ezz = 02 ] || [ $ezz = 2 ]
+then
+clear
+echo "\033[36;1m"
+toilet -f  mono12 -F gay "TELKOMSEL"
+php telkomsel.php
+fi
+
+if [ $ezz = 03 ] || [ $ezz = 3 ]
+then
+clear 
+echo  "\033[39;1m"
+toilet -f mono12 -F metal "PHD"
+php phd.php
+fi
+
+if [ $ezz = 04 ] || [ $ezz = 4 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "MATAHARIMALL"
+php mataharimall.php
+fi
+
+if [$ezz = 05 ] || [ $ezz = 5 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "GRAB"
+python2 spammer.py
+fi
+
+if [ $ezz = 06 ] || [ $ezz = 6 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "TOKPED"
+php tokped.php
+fi
+
+if [ $ezz = 07 ] || [ $ezz = 7 ]
+then 
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "INFO"
+echo "Dibuat oleh "
+echo "AMRiezz | Ryuki"
+echo "versi 0.1"
+echo "THANKS TO ALL MEMBER GCO"
+echo ""
+fi
+
+
+if [ $ezz = 08 ] || [ $ezz =8 ]
+then
+clear
+echo "Loading ----------------------------------"|lolcat
+sh install.sh
+sh gco.sh
+fi
+
+if [ $ezz = 09 ] || [ $ezz =9 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "KELUAR"
+echo "semoga hari mu menyenangkan"
+fi
+
